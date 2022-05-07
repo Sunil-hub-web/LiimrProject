fd5507f9c4a58de5850a227e376063293a885c76
dell <sunilkumard628@gmail.com>
2022-05-07 18:27:45 +0530

    add

---
+++ app/src/main/java/com/in/apnisevatechinican/TermsofService.java
@@ -0,0 +1,42 @@
+package com.in.apniseva.activity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.in.apniseva.R;
+
+public class TermsofService extends AppCompatActivity {
+
+    TextView text;
+    ImageView image_back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_termsof_service);
+
+        text = findViewById(R.id.text);
+        image_back = findViewById(R.id.image_back);
+
+        image_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+              onBackPressed();
+            }
+        });
+
+        text.setText("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+      finish();
+    }
+}
